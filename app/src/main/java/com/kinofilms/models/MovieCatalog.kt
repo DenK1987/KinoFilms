ff955c61631bd0e1d34df9e9d810526056540c30
DenKli <https://github.com/DenK1987/AN10-onl>
2023-02-12 19:46:34 +0300

    Commit3.1

---
+++ app/src/main/java/com/kinofilms/models/MovieCatalog.kt
@@ -0,0 +1,15 @@
+package com.kinofilms.models
+
+data class MovieCatalog(
+    val id: Int,
+    val name: String,
+    val year: String,
+    val ratingKp: String,
+    val imageUrl: String,
+    val errorMessage: String
+)
+
+data class AllMoviesCatalog(
+    val data: List<MovieCatalog>?,
+    var error: String = ""
+)
